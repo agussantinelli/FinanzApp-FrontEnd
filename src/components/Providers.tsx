a96983b951345bdde29f04cf096bd557908388b2
Agustin Santinelli <agustinsantinelli@gmail.com>
2025-11-02 03:16:33 -0300

    Corrección Providers

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,19 +1,13 @@
-// app/providers.tsx
 "use client";
 
-import * as React from "react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import theme from "@/theme";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return {
-  }(
-    <AppRouterCacheProvider options={{ key: "mui" }}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </AppRouterCacheProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
   );
 }
