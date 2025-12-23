8b15b11014bcc068a3172c035fabd2cd6c97deb2
Agustin Santinelli <agustinsantinelli@gmail.com>
2025-12-22 22:52:21 -0300

    NUEVO: Floating Message

---
+++ src/components/ui/FloatingMessage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Snackbar, Alert, AlertColor } from "@mui/material";
+
+interface FloatingMessageProps {
+    open: boolean;
+    message: string | null;
+    severity: AlertColor;
+    onClose: () => void;
+}
+
+export default function FloatingMessage({ open, message, severity, onClose }: FloatingMessageProps) {
+    return (
+        <Snackbar
+            open={open}
+            autoHideDuration={6000}
+            onClose={onClose}
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        >
+            <Alert onClose={onClose} severity={severity} sx={{ width: "100%", boxShadow: 3 }}>
+                {message}
+            </Alert>
+        </Snackbar>
+    );
+}
