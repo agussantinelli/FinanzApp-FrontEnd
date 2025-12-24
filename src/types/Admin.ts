12927a2e89ef58f3bf3ee4efb01d69b299347907
Agustin Santinelli <agustinsantinelli@gmail.com>
2025-12-24 16:55:19 -0300

    corrijo types

--- src/types/Admin.ts
+++ src/types/Admin.ts
@@ -1,9 +1,30 @@
-export interface AdminDashboardStats {
+export interface AdminStatsDTO {
     totalUsuarios: number;
-    usuariosHoy: number;
-    totalOperaciones: number;
-    operacionesHoy: number;
-    volumenHoyArs: number;
-    volumenHoyUsd: number;
+    nuevosUsuariosMes: number;
     totalActivos: number;
+    recomendacionesPendientes: number;
+    efectividadGlobal: string; // "-" default
+}
+
+export interface AssetPopularityDTO {
+    symbol: string;
+    nombre: string;
+    cantidadInversores: number;
+    valorTotalPesos: number;
+}
+
+export interface GlobalDistributionDTO {
+    tipoActivo: string;
+    valorPesos: number;
+    porcentaje: number;
+}
+
+export interface AdminPortfolioStatsDTO {
+    totalPortafolios: number;
+    totalInversoresConTenencia: number;
+    valorGlobalPesos: number;
+    valorGlobalDolares: number;
+    variacionPromedioDiaria: number;
+    activosMasPopulares: AssetPopularityDTO[];
+    distribucionPorTipo: GlobalDistributionDTO[];
 }
