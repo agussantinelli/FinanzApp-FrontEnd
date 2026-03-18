da89481885ac6d063532c65f8e4dd80a8ffef3ed
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-18 19:59:33 -0300

    Mejora Config Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: 1,
-    reporter: 'html',
+    timeout: 60000,
+    expect: {
+        timeout: 10000,
+    },
     use: {
         baseURL: 'http://localhost:3000',
         trace: 'on-first-retry',
